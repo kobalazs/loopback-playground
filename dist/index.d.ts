56234cbe92acd4ba319cdcb0fd90f742e65aec2b
Balázs Kovács <kobalazs@gmail.com>
2020-07-09 11:04:51 +0200

    Add dist & tsconfig.tsbuildinfo to .gitignore

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ApplicationConfig, LoopbackPlaygroundApplication } from './application';
-export * from './application';
-export declare function main(options?: ApplicationConfig): Promise<LoopbackPlaygroundApplication>;
